f41312138a9d4b51939154c4b26940c531beed7e
Mena Simão <mena.simao@datapack.internal>
2022-06-21 13:37:03 -0300

    second  commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'https://gabbyblog.herokuapp.com/';
 
 Vue.config.productionTip = false
-
 new Vue({
+  store,
   router,
   render: h => h(App)
 }).$mount('#app')
